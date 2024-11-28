84983f26ca3669f4123e238d5db490634b33139f
Sak-drago <sakshatsachdeva@gmail.com>
2024-11-29 00:41:58 +0530

    Task Update: Added Random Auto Filling

--- fill.js
+++ fill.js
@@ -1,6 +1,11 @@
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 let buttons = document.getElementsByClassName('radio-inline');
-for(let i = 0; i < buttons.length; ++i){
-    if(5 - (i % 5) == 3){ // change to 1,2,3 or 4 for different default response
+for (let i = 0; i < buttons.length; ++i) {
+    let _randomNum = randomInt(1, 5); // Random number between 1 and 5
+    if (5 - (i % 5) == _randomNum) { //If you want to set a default value, change the value of _randomNum to the value you want
         buttons[i].click();
     }
 }
