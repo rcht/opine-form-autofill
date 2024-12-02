1c30b5724940717a8cec374fc3c1092a2d6a20d6
taraash23552 <162435153+taraash23552@users.noreply.github.com>
2024-12-02 16:53:44 +0530

    A better random clicker
    Works 100% of the time

--- fill.js
+++ fill.js
@@ -1,6 +1,4 @@
 let buttons = document.getElementsByClassName('radio-inline');
-for(let i = 0; i < buttons.length; ++i){
-    if(5 - (i % 5) == 3){ // change to 1,2,3 or 4 for different default response
-        buttons[i].click();
-    }
+for(let i = 0; i < buttons.length; i = i+5){
+   buttons[i + Math.floor(Math.random() * 5)].click();
 }
